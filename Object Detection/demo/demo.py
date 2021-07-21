22357890c25b8123a986cc1b617896e0a1cb14d9
chinghancheng <helen3909@gmail.com>
2021-07-21 21:32:01 +0800

    Add Object detection HW

---
+++ Object Detection/demo/demo.py
@@ -0,0 +1,22 @@
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+import numpy as np
+import cv2
+if torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+from ssd import build_ssd
+
+net = build_ssd('test', 300, 21)    # initialize SSD
+
+print(net)
+
+net.load_weights('ssd300_mAP_77.43_v2.pth')
+
---
+++ Object Detection/test.py
@@ -0,0 +1,17 @@
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+import numpy as np
+import cv2
+if torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+from ssd import build_ssd
+
+
